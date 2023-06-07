9fec861e922b0c94a3a4836203fe3c0669d90ff4
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-06-07 19:57:26 +0530

    variable declaration.

---
+++ 03-vars/vars.tf
@@ -0,0 +1,16 @@
+#variable declaration
+variable "sample" {
+  default = 100
+}
+variable "sample1" {
+  default = "hello world"
+}
+output "sample" {
+  value = var.sample
+}
+output "sample1" {
+  value = var.sample1
+}
+output "sample-ext" {
+  value = "value of sample - ${var.sample}"
+}

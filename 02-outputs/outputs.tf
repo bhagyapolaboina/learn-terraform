5e299de787056f8f1f0e6587582ad1e773b21f64
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-06-07 19:51:56 +0530

    create instance using terraform.output.

--- 02-outputs/outputs.tf
+++ 02-outputs/outputs.tf
@@ -3,7 +3,7 @@ output "sample" {
 }
 #any string in terraform need to be quoted with double quotes only.
 #single quotes are not supported
-output "sample" {
+output "sample1" {
   value = 1000
 }
 #number and booleans (true,false)

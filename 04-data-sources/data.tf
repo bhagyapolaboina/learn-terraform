8e7c1a0d0f6c0be6353d26a9ee67be79397539a9
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-06-07 21:14:04 +0530

    variable declaration.vars.data sourse.

--- 04-data-sources/data.tf
+++ 04-data-sources/data.tf
@@ -10,9 +10,11 @@ data "aws_ec2_spot_price" "example" {
 output "price" {
   value = data.aws_ec2_spot_price.example.spot_price
 }
- data "aws_security_group" "selected" {
-  name= "allow all"
-}
-output "sgid" {
-  value = data.aws_security_group.selected.id
+  data "aws_ami" "example" {
+    owners=[106797252878]
+    most_recent = true
+    name_regex = "workstation"
+  }
+output "ami" {
+  value = data.aws_ami.example.id
 }

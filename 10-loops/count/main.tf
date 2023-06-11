e971ae6aedf7376c86a6170e32aa2d6d73633df4
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-06-11 11:26:54 +0530

    element function.selected..

--- 10-loops/count/main.tf
+++ 10-loops/count/main.tf
@@ -1,17 +1,14 @@
 resource "aws_instance" "web" {
-  count         = length(var.instances)
+  count = 3
   ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
 
   tags = {
-    Name = "hello world "
+    Name = "HelloWorld"
   }
 }
 data "aws_ami" "example" {
   owners=["106797252878"]
   most_recent = true
   name_regex = "workstation"
-}
-variable "instances" {
-  default = ["fronted,catalogue,cart"]
 }

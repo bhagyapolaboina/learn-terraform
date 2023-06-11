3c9fb90a94f6c6f5f7b264f8630730173f850780
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-06-11 12:42:49 +0530

    element function.selected..

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -3,4 +3,19 @@ module "instnaces" {
   source = "./ec2"
   name = each.key
 }
+variable "instances" {
+  default = {
+    fronted = {}
+    mongodb = {}
+    catalogue = {}
+    cart = {}
+    user = {}
+    mysql = {}
+    rabbitmq = {}
+    payments = {}
+    redis = {}
+    shipping = {}
+  }
+
+}
 

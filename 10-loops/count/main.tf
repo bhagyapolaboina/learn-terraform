30a8b05bdc2d47dbf37c486aec3efdb76b350c20
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-06-11 10:04:52 +0530

    creating three instances.

---
+++ 10-loops/count/main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "web" {
+  count = 3
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+data "aws_ami" "example" {
+  owners=[106797252878]
+  most_recent = true
+  name_regex = "workstation"
+}

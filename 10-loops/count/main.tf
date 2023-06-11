105308e803be838b5999e21d52a4410d2bbd6647
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-06-11 12:06:31 +0530

    element function.selected..

--- 10-loops/count/main.tf
+++ 10-loops/count/main.tf
@@ -1,10 +1,19 @@
+//resource "aws_instance" "web" {
+ // count = 3
+  //ami           = data.aws_ami.example.id
+  //instance_type = "t3.micro"
+
+  //tags = {
+    //Name = "HelloWorld-${count.index}"
+  //}
+//}
 resource "aws_instance" "web" {
   count = 3
   ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld-${count.index}"
+    Name = var.instances[count.index]
   }
 }
 data "aws_ami" "example" {

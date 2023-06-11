b3f077ff6f4961c29a9bb6acf48c19e604f8cda6
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-06-11 12:54:02 +0530

    element function.selected..

--- 11-functions/main.tf
+++ 11-functions/main.tf
@@ -25,10 +25,16 @@
      output "devops_topics" {
        value = var.classes["devops"]["topics"]
      }
-     output "aws_topics" {
-       value = lookup(var.classes,"aws","null")
-     }
+     //output "aws_topics" {
+       //value = lookup(var.classes,"aws","null")
+    // }
      output "fruit_4" {
        value = element(var.fruits,4)
+     }
+     variable "a" {
+       default = 100
+     }
+     output "a" {
+       value = var.a > 100 ? "surplus quantity" : "less quantity"
      }
 

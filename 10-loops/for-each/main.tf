2150a6777d9a66c4c9ddba5db259efa718468469
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-06-11 12:18:12 +0530

    element function.selected..

---
+++ 10-loops/for-each/main.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "web" {
+  count = 3
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = var.instances[count.index]
+  }
+}
+data "aws_ami" "example" {
+  owners      = ["106797252878"]
+  most_recent = true
+  name_regex  = "workstation"
+}
+variable "instances" {
+  default = {
+    fronted = {
+      name = "fronted"
+      instance_type = "t3.micro"
+    }
+    catalogue = {
+      name = "catalogue"
+      instance_type = "t3.micro"
+    }
+    cart = {
+      name = "cart"
+      instance_type = "t3.micro"
+    }
+
+  }
+}

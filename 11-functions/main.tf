9702f9b9c7787a849fb90b3e150a536ce32a94d2
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-06-11 11:46:13 +0530

    element function.selected..

--- 11-functions/main.tf
+++ 11-functions/main.tf
@@ -5,3 +5,9 @@
      output "class" {
        value = upper(var.class)
      }
+     variable "fruits" {
+       default = ["apple,banana,orange"]
+     }
+     output "fruit-count" {
+       value = length(var.fruits)
+     }

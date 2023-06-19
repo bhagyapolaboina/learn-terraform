19e500d023587c9b50e70605444ac137c0a7ef1d
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-06-19 12:44:37 +0530

    terraform basics

--- 04-data-sources/data.tf
+++ 04-data-sources/data.tf
@@ -11,3 +11,20 @@ output "price" {
   value = data.aws_ec2_spot_price.example.spot_price
 }
 
+data "aws_security_group" "selected" {
+  name= "allow-all"
+}
+output "sgid" {
+  value = data.aws_security_group.selected.id
+}
+data "aws_ami" "example" {
+  owners=["106797252878"]
+  most_recent = true
+  name_regex = "workstation"
+}
+output "ami" {
+  value = data.aws_ami.example.id
+}
+provider "aws" {
+  region="us-east-1"
+}

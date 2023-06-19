c0a2a4046a60d6c5e9a4ff276fadfdeb959000c3
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-06-19 11:42:31 +0530

    terraform basics

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -1,6 +1,6 @@
 #variable declaration
 variable "sample" {
-  default = 100
+  default = 200
 }
 variable "sample1" {
   default = "hello world"
@@ -14,54 +14,4 @@ output "sample1" {
 output "sample-ext" {
   value = "value of sample - ${var.sample}"
 }
-#variable data types
-#1.string 2.number 3.boolean
-#variable types
-#in ansible 1.plain key, value 2.list 3.map/directory
-#in terraform 1.plain 2.list 3.map
 
-
-#plain
-variable "course" {
-  default = "devops training"
-}
-#list
-variable "courses" {
-  default = [
-    "devops",
-    "aws",
-    "python"
-  ]
-}
-#map
-variable "course_details" {
-  default = {
-    devops={
-      name= "devops"
-      Timing= "10am"
-      Duration= 90
-    }
-    AWS={
-      name= "aws"
-      Timing= "11am"
-    }
-  }
-}
-output "course" {
-  value = var.course
-}
-output "courses" {
-  value = var.courses
-}
-output "course_details" {
-  value = var.course_details
-}
-variable "env" {}
-  output "env" {
-    value = var.env
-  }
-
-variable "url" {}
-output "url" {
-  value = var.url
-}

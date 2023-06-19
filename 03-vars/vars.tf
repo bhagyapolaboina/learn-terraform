a0e75de3bde7d537add0f256a41ed06e1ec8ee1d
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-06-19 11:54:27 +0530

    terraform basics

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -11,7 +11,52 @@ output "sample" {
 output "sample1" {
   value = var.sample1
 }
+#some times if variable/any reference with the combination of some other string then we have to access those in ${}
 output "sample-ext" {
   value = "value of sample - ${var.sample}"
 }
+
+#variable data types
+#1.string 2.number 3.boolean
+#variable types
+#in ansible 1.plain key, value 2.list 3.map/directory
+#in terraform 1.plain 2.list 3.map
+
+
+#plain
+variable "course" {
+  default = "devops training"
+}
+#list
+variable "courses" {
+  default = [
+    "devops",
+    "aws",
+    "python"
+  ]
+}
+#map
+variable "course_details" {
+  default = {
+    devops={
+      name= "devops"
+      Timing= "10am"
+      Duration= 90
+    }
+    AWS={
+      name= "aws"
+      Timing= "11am"
+    }
+  }
+}
+output "course" {
+  value = var.course
+}
+output "courses" {
+  value = var.courses
+}
+output "course_details" {
+  value = var.course_details
+}
+
 
